545105de837b15f988f2c310396581df0b9d5fee
LAPTOP-TCQR380M\pasqu <pasquale.russo@cws.it>
2023-10-20 14:57:37 +0200

    eureka_microoservices

---
+++ src/test/java/realt/corso/microservizi/server/PhotoAppDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package realt.corso.microservizi.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
