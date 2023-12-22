22f82ac10ff2ff3465fc90717743e119cfc65ea0
LAPTOP-TCQR380M\pasqu <pasquale.russo@cws.it>
2023-12-22 17:59:07 +0100

    filter_cors

--- src/main/java/realt/corso/microservizi/api/ApiGatewayApplication.java
+++ src/main/java/realt/corso/microservizi/api/ApiGatewayApplication.java
@@ -2,7 +2,7 @@ package realt.corso.microservizi.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 @SpringBootApplication
 public class ApiGatewayApplication {
 
